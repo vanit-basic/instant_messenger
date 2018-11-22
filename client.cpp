d703328c0eebd92e4821af0a5d4615aece010c48
Hrach007 <hrach.asatryan.1998@mail.ru>
2018-11-23 03:38:28 +0400

    Wronge codegit add client.cpp connection.cpp connection.hpp server.cpp user.cpp user.hpp verification.cpp verification.hpp !

--- client.cpp
+++ client.cpp
@@ -4,115 +4,164 @@
 #include "verification.hpp"
 #include <mutex>
 #include <string>
+std::string for_login="";
 std::string fifo1="";
 std::string fifo2="";
 connection* mainConnection = NULL;
 
+
 void recv_message_c(connection* c, std::string message) {
+	std::string msg1="";
+	std::string info=":action:registration";
+	std::string confmsg="";
+	std::string msg="";
 
-        std::cout << "From server : " << message << std::endl;
-        std::string msg="";
-        std::string info="";
-        std::string confmsg="";
+	std::cout << "From server : " << message << std::endl;
+	if(message=="<registration> or <sign in>" || message=="You allowed following action: send message another user, create a new group, send message to group or quit" || message=="There is no such combination of login and password!")
 
-        if(message=="<registration> or <sign in>")
-        {
-                do
-                {
-                        std::cout<<"Enter action!\n";
-                        getline(std::cin,msg);
-                        if(msg!="registration" && msg!="sign in")
-                                std::cout<<"Wrong action!";
-                }
-                while(msg!="registration" && msg!="sign in");
-                c->send(msg);
-                return;
-        }
+	{
+		do
+		{
+			std::cout<<"Enter action!\n";
+			getline(std::cin,msg1);
+			if(msg1!="registration" && msg1!="sign in" || msg1=="send" || msg1=="id")
+				std::cout<<"Wrong action!";
+		}
+		while(msg1!="registration" && msg1!="sign in" &&  msg1!="send" && msg1!="id");
+	}
+	/*	std::cout<<"Enter action!\n";
+		getline(std::cin,msg1);
+		if(msg1=="registration" || msg1=="sign in" || msg1=="send")
+			break;
+		else
+			std::cout<<"Wrong action!\n";*/
+	if(msg1=="id")
+		c->send("show users id");
 
-        if(message=="Enter your information")
-        {
-                std::string msg;
-                do
-                {
-                        std::cout<<"Firstname: ";
-                        getline(std::cin,msg);
-                        if(!isvalid_firstname(msg))
-                                std::cout<<"Invalid firstname!\n";
-                }
-                while(!isvalid_firstname(msg));
-                info+=":firstname:"+msg;
+	std::string user_id="";
+	std::string text="";
+	if(msg1=="send")
+	{
+		//c->send("show users id");
+		do
+		{
+			std::cout<<"Enter user id : ";
+			getline(std::cin,id);
+		}while();
+		c->send();
+	}
+	
 
-                do
-                {
-                        std::cout<<"Lastname: ";
-                        getline(std::cin,msg);
-                        if(!isvalid_lastname(msg))
-                                std::cout<<"Invalid lastname!\n";
-                }
-                while(!isvalid_lastname(msg));
-                info+=":lastname:"+msg;
 
-                do
-                {
-                        std::cout<<"Mail: ";
-                        getline(std::cin,msg);
-                        if(!isvalid_mail(msg))
-                                std::cout<<"Invalid mail!\n";
-                }
-                while(!isvalid_mail(msg));
-                info+=":mail:"+msg;
+	}
+	if(msg1=="registration" || message=="Login is busy!")
+	{
 
-                do
-                {
-                        std::cout<<"Birth date: ";
-                        getline(std::cin,msg);
-                        if(!isvalid_birth_date(msg))
-                                std::cout<<"Invalid birth date!\n";
-                }
-                while(!isvalid_birth_date(msg));
-                info+=":birth_date:"+msg;
+		do
+		{
+			std::cout<<"Firstname: ";
+			getline(std::cin,msg);
+			if(!isvalid_firstname(msg))
+				std::cout<<"Invalid firstname!\n";
+		}
+		while(!isvalid_firstname(msg));
+		info+=":firstname:"+msg;
 
-                do
-                {
-                        std::cout<<"Gender: ";
-                        getline(std::cin,msg);
-                        if(!isvalid_gender(msg))
-                                std::cout<<"Invalid gender!\n";
-                }
-                while(!isvalid_gender(msg));
-                info+=":gender:"+msg;
+		do
+		{
+			std::cout<<"Lastname: ";
+			getline(std::cin,msg);
+			if(!isvalid_lastname(msg))
+				std::cout<<"Invalid lastname!\n";
+		}
+		while(!isvalid_lastname(msg));
+		info+=":lastname:"+msg;
 
-                do
-                {
-                        std::cout<<"Login: ";
-                        getline(std::cin,msg);
-                        if(!isvalid_login(msg))
-                                std::cout<<"Invalid login!\n";
-                }
-                while(!isvalid_login(msg));
-                info+=":login:"+msg;
+		do
+		{
+			std::cout<<"Mail: ";
+			getline(std::cin,msg);
+			if(!isvalid_mail(msg))
+				std::cout<<"Invalid mail!\n";
+		}
+		while(!isvalid_mail(msg));
+		info+=":mail:"+msg;
 
-                do
-                {
-                        do
-                        {
-                                std::cout<<"Password: ";
-                                getline(std::cin,msg);
-                                if(!isvalid_password(msg))
-                                        std::cout<<"Invalid password!\n";
-                        }
-                        while(!isvalid_password(msg));
+		do
+		{
+			std::cout<<"Birth date: ";
+			getline(std::cin,msg);
+			if(!isvalid_birth_date(msg))
+				std::cout<<"Invalid birth date!\n";
+		}
+		while(!isvalid_birth_date(msg));
+		info+=":birth_date:"+msg;
 
-                        std::cout<<"Confirm password: ";
-                        getline(std::cin,confmsg);
-                }
-                while(msg!=confmsg);
-                info+=":password:"+msg;
-                info+=":";
+		do
+		{
+			std::cout<<"Gender: ";
+			getline(std::cin,msg);
+			if(!isvalid_gender(msg))
+				std::cout<<"Invalid gender!\n";
+		}
+		while(!isvalid_gender(msg));
+		info+=":gender:"+msg;
 
-                c->send(info);
-                return;
-        }
+		do
+		{
+			do
+			{
+				std::cout<<"Password: ";
+				getline(std::cin,msg);
+				if(!isvalid_password(msg))
+					std::cout<<"Invalid password!\n";
+			}
+			while(!isvalid_password(msg));
+
+			std::cout<<"Confirm password: ";
+			getline(std::cin,confmsg);
+		}
+		while(msg!=confmsg);
+		info+=":password:"+msg;
+
+		do
+		{
+			std::cout<<"Login: ";
+			getline(std::cin,msg);
+			if(!isvalid_login(msg))
+				std::cout<<"Invalid login!\n";
+		}
+		while(!isvalid_login(msg));
+		info+=":login:"+msg+ ":";
+		c->send(info);
+}
+
+///////
+if(msg1=="sign in")
+{
+	std::string log="";
+	std::string pass="";
+	do
+	{
+		std::cout<<"Login: ";
+		getline(std::cin,log);
+		if(!isvalid_login(log))
+			std::cout<<"Invalid login!\n";
+	}
+	while(!isvalid_login(log));
+	do
+	{
+		std::cout<<"Password: ";
+		getline(std::cin,pass);
+		if(!isvalid_password(pass))
+			std::cout<<"Invalid password!\n";
+	}
+	while(!isvalid_password(pass));
+	std::string sign=":action:sign_in:"+log+":"+pass;
+	c->send(sign);
+}
+
+	
 }
 
 void binder_recv_message(connection* c, std::string message) {
--- server.cpp
+++ server.cpp
@@ -8,10 +8,22 @@
 #include <string>
 #include <list>
 #include <map>
+static int value=0;
+std::list<connection*> connections;
 static int id=9;
 std::map<std::string,std::string> current_user;
 std::map<std::string,std::pair<std::string,std::string>> user_info;
 std::map<int,user> id_ob;
+std::map<int,connection*> id_con;
+std::string show_id()
+{
+	std::string id="Show id : ";
+	std::map<int,user>::iterator it;
+	for(it=id_ob.begin();it!=id_ob.end();++it)
+		id+=it->first + " ";
+	return id;
+
+}
 bool search_log(std::string log)
 {
         std::map<std::string,std::pair<std::string,std::string>>::iterator it;
@@ -19,6 +31,17 @@ bool search_log(std::string log)
                 if(it->first==log)
                         return false;
         return true;
+}
+bool search_log_passw(std::string str)
+{
+	std::string log=str.substr(0,str.find(":"));
+	std::string passw=str.substr(str.find(":")+1);
+	std::cout<<"login: "<<log<<"Password: "<<passw<<std::endl;
+	if(user_info[log].second==passw)
+		return true;
+	return false;
+
+
 }
 std::map<std::string,std::string> string_to_map(std::string str)
 {
@@ -46,10 +69,7 @@ std::string map_to_string(std::map<std::string,std::string> mymap)
         return str;
 }
 
-static int value=0;
 
-std::list<connection*> connections;
-std::string str="";
 void recieve(connection* c, std::string msg) {
         std::cout << "from client : " << msg << std::endl;
         if(":action:help:"==msg)
@@ -62,31 +82,50 @@ void recieve(connection* c, std::string msg) {
                 c->send("Enter your information");
                 return;
         }
+	if(msg=="show users id")
+	{
+		c-send(show_id());
+	}
+	if(msg.substr(0,6)=="login:")
+	{
+		if(!search_log(msg.substr(7)))
+			c->send("Invalid login");
+		else
+			c->send("Valid login");
+	}
+	if(msg.find(":action:registration")!=std::string::npos)
+	{
+		msg.erase(0,20);
+		current_user=string_to_map(msg);
+		if(search_log(current_user["login"]))
+		{
 
-        if(msg[0]==':')
-        {
-                current_user=string_to_map(msg);
-                if(search_log(current_user["login"]))
-                {
-
-                        ++id;
+			++id;
                         current_user.insert(std::pair<std::string,std::string>("id",std::to_string(id)));
                         user ob(current_user);
                         id_ob.insert(std::pair<int,user>(id,ob));
                         user_info.emplace(current_user["login"],std::make_pair(std::to_string(id),current_user["password"]));
-                        str="your id : "+ std::to_string(id);
+			id_con.emplace(id,c);
+			std::string str="your id : "+ std::to_string(id);
                         c->send(str);
+			 c->send("You allowed following action: send message another user, create a new group, send message to group or quit");
                         return;
                 }
-                else
-                {
-                        c->send("The login coincides\n<registration> or <log in>\n");
-                        return;
-                }
+		else
+			c->send("Login is busy!");
         }
-        else
+        if(msg.find(":action:sign_in")!=std::string::npos)
         {
-                c->send("Error message!\n<registration> or <log in>\n");
+		msg.erase(0,16);
+		if(!search_log_passw(msg))
+			c->send("There is no such combination of login and password!");
+		else
+		{
+			std::string s="your information "+ map_to_string(current_user);
+
+			c->send("You allowed following action: send message another user, create a new group, send message to group or quit");
+
+		}
                 return;
         }
 
