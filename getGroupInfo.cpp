8e064b908cacdfea89fcd2e32210782a989795dc
Hrach007 <hrach.asatryan.1998@mail.ru>
2018-12-11 23:57:10 +0400

    Add getGroupInfo.cpp

---
+++ getGroupInfo.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<string>
+#include <stdio.h>
+#include<fstream>
+
+std::string getGroupInfo(std::string group_id)
+{
+	std::string str="";
+	std::string res="";
+	std::ifstream xml("db_files/groups/g"+group_id+"/ginfo.xml");
+	while(xml>>str)
+		res+=str;
+	return res;
+}
+
+
+
+int main()
+{
+
+	std::string id="";
+	std::getline(std::cin,id);
+	std::cout<<getGroupInfo(id)<<std::endl;	
+	return 0;
+}
