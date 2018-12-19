1b94e895eb21b1e6031f25047e9345a1f3d55046
Davit Grigoryan <grig.davit@gmail.com>
2018-12-19 18:28:06 +0400

    Added fileManager class to move all file manipulation functionalities

---
+++ headers/fileManager.hpp
@@ -0,0 +1,29 @@
+#include <string>
+#include <vector>
+
+class fileManager {
+
+	public:
+
+		void getFileContent(std::string path, std::string& content);
+		void getDirectoryContent(std::string path, std::vector<std::string>& files);
+
+
+		bool isFileExist(std::string path);
+		bool isDirectory(std::string path);
+		bool isRegularFile(std::string path);
+		bool isSymLink(std::string path);
+
+		int deleteFolder(std::string path, bool recursive);
+		int deleteFile(std::string path);
+		int createSymlink(std::string filePath, std::string linkPath);
+		int createFile(std::string path);
+		int createFolder(std::string path);
+
+	public:
+		static fileManager* sharedManager();
+
+	public:
+		fileManager();
+		~fileManager();
+};
