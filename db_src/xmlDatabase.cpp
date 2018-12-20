6e888ae912c5561089519c4ed307c12f75617076
Hrach007 <hrach.asatryan.1998@mail.ru>
2018-12-20 15:54:59 +0400

    Add test_ChangeGroupAdmin()

--- db_src/xmlDatabase.cpp
+++ db_src/xmlDatabase.cpp
@@ -794,7 +794,7 @@ bool changeAdmin(std::string gId,std::string newAdmin){
 		}
 	}
 	xmlFreeDoc(doc);
-	path = "db_files/users" + oldAdmin + "/info.xml";
+	path = "db_files/users/" + oldAdmin + "/info.xml";
 	doc = xmlReadFile(path.c_str(), NULL, 0);
 	root = xmlDocGetRootElement(doc);
 	for(node = root->children; node; node = node->next){
@@ -811,7 +811,7 @@ bool changeAdmin(std::string gId,std::string newAdmin){
 	}
 	xmlSaveFormatFileEnc(path.c_str(), doc, "UTF-8", 0);
 	xmlFreeDoc(doc);
-	path = "db_files/users" + newAdmin + "/info.xml";
+	path = "db_files/users/" + newAdmin + "/info.xml";
 	doc = xmlReadFile(path.c_str(), NULL, 0);
 	root = xmlDocGetRootElement(doc);
 	for(node = root->children; node; node = node->next){
