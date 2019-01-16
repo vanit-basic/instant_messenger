db1894b6ab1c575619d4e8da1671dcaf36204ac9
NarekJilavyan <narek_jilavyan@mail.ru>
2019-01-16 21:52:38 +0400

    request response headers add

---
+++ headers.hpp
@@ -0,0 +1,35 @@
+class request {
+	private:
+		std::string type;
+		std::string url;
+		std::string header;
+		std::string body;
+	public:
+		void setType(std::string);
+		void setUrl(std::string);
+		void setHeader(std::string);
+		void setBody(std::string);
+		std::string getType();
+		std::string getUrl();
+		std::string getHeader();
+		std::string getBody();
+		request(std::string);
+		~request();
+}
+
+
+class response {
+	private:
+		std::string url;
+		std::string header;
+		std::string body;
+	public:
+		void setUrl(std::string);
+		void setHeader(std::string);
+		void setBody(std::string);
+		std::string getUrl();
+		std::string getHeader();
+		std::string getBody();
+		response(std::string);
+		~response();
+}
