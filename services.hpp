0459f2fca6103e5cd44c328264923950a7fe1b79
NarekJilavyan <narek_jilavyan@mail.ru>
2019-01-18 22:21:06 +0400

    Service class

--- services.hpp
+++ services.hpp
@@ -1,18 +1,17 @@
 #ifndef SERVICES_HPP
 #define SERVICES_HPP
 class Service {
-
-        public:
-                connection DBconnection;
-                virtual void send(std::string) = 0;
-                virtual std::string recive() = 0;
-                virtual std::string getAction(std::string) = 0;
-                Service(std::string url);
-                ~Service();
+	public:
+		connection DBconnection;
+		void send(std::string);
+		std::string recive();
+		std::string getAction(std::string);
+		Service(std::string url);
+		~Service();
 }
-class Authetification:public Service {
 
-        public:
+class Authetification:public Service {
+	public:
 		std::string registerUser(std::string userInfo);
 		std::string loginUser (std::string login, std::string password);
 		std::string forgotPassword(std::string Email);
@@ -21,8 +20,8 @@ class Authetification:public Service {
 		Authetification(std::string url);
 		~Authetification();
 }
+
 class Conversation:public Service {
-	
 	public:	
 		bool addUserMessage(std::string from,std::string to,std::string massageInfo);
 		bool removeUserConversation(std::string fromUserId,std::string toUserId);
@@ -33,13 +32,12 @@ class Conversation:public Service {
 		std::string getUserConversations(std::string userId);
 		std::string getGroupconversation (std::string userId,std::string groupId);
 		std::string updateGroupMessage (std::string groupId,std::sstring messBody);
-		
+
 		Conversation(std::string url);
 		~Conversation();
 }
 
 class Account:public Service{
-	
 	public:
 		bool deleteUser (std::string userId);
 		bool deleteGroup (std::string groupId);
@@ -47,25 +45,26 @@ class Account:public Service{
 		bool updateGroupInfo(std::string groupInfo);
 		bool addUserToGroup(std::string groupId,std::string userId);
 		bool removeFromGroup(std::string groupId,std::string userId);
-                bool singnOut(std::string userId);
-                std::string getUserInfo(std::string userId);
+		bool singnOut(std::string userId);
+		std::string getUserInfo(std::string userId);
 		std::string getUserShortInfo(std::string userId);
 		std::string getGroupInfo(std::string groupId);
 		std::string getGroupUsers(std::string groupId);
 		std::string createGroup(std::string groupInfo);
-		
+
 		Account(std::string url);
 		~Account();
 
 }
+
 class Game:public Service {
-
-        public:
+	public:
 
 }
+
 class Notification:public Service {
-
-        public:
+	public:
 
 }
+
 #endif
