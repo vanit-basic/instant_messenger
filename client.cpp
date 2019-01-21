b8ac0e3be90e21e225c7c977f0991f465419c005
narekayd091 <narekayd091@gmail.com>
2019-01-22 00:34:35 +0400

    Sxalneri uxxum serveri ev clienti mej

--- client.cpp
+++ client.cpp
@@ -4,38 +4,40 @@
 #include <sys/un.h>
 #include <unistd.h>
 #include <iostream>
+#include <string>
+// Write TEXT to the socket given by file descriptor SOCKET_FD.
 
-/* Write TEXT to the socket given by file descriptor SOCKET_FD.
-*/
 void write_text (int socket_fd, const char* text)
 {
-/* Write the number of bytes in the string, including
-NUL-termination. */
-int length = strlen (text) + 1;
-write (socket_fd, &length, sizeof (length));
-/* Write the string. */
-write (socket_fd, text, length);
+	// Write the number of bytes in the string, including NUL-termination. 
+	int length = strlen (text) + 1;
+	write (socket_fd, &length, sizeof (length));
+	// Write the string.
+	write (socket_fd, text, length);
 }
 int main (/*int argc, char* const argv[]*/)
 {
-	const char* p = "/home/narek/instant_messenger/socket";
-const char* const socket_name = p;
-char * m ;
-std::cin>>m;
-const char* const message = m;
-int socket_fd;
-struct sockaddr_un name;
-/* Create the socket. */
-socket_fd = socket (PF_LOCAL, SOCK_STREAM, 0);
-/* Store the server's name in the socket address. */
-name.sun_family = AF_LOCAL;
-strcpy (name.sun_path, socket_name);
-/* Connect the socket.
-*/
-connect (socket_fd, (struct sockaddr*)&name, SUN_LEN (&name));
-/* Write the text on the command line to the socket.
-write_text (socket_fd, message);
-close (socket_fd);
-return 0;
-*/
+	const char* p = "/home/narek/Documents/Tnayin/instant_messenger/socket";
+	const char* const socket_name = p;
+	std::string  m="";
+	while (! (m == "close"))
+	{
+		std::cout<<"Mutqagreq uxarkvox informacian\n";
+		std::cin>>m;
+		const char* message = m.c_str();
+		int socket_fd;
+		struct sockaddr_un name;
+		// Create the socket. 
+		socket_fd = socket (PF_LOCAL, SOCK_STREAM, 0);
+		// Store the server's name in the socket address.
+		name.sun_family = AF_LOCAL;
+		strcpy (name.sun_path, socket_name);
+		// Connect the socket.
+		connect (socket_fd, (struct sockaddr*)&name, SUN_LEN (&name));
+		// Write the text on the command line to the socket.
+		write_text (socket_fd, message);
+		close (socket_fd);
+	}
+	return 0;
+
 }
