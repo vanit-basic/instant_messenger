61c0b5501a6389ac7bb2fb480e5af8792d0158cb
narekayd091 <narekayd091@gmail.com>
2019-01-22 22:26:36 +0400

    delete excess

--- client.cpp
+++ client.cpp
@@ -9,36 +9,22 @@
 
 void write_text (int socket_fd, const char* text)
 {
-	std::cout<<"Write skizb\n";
 	// Write the number of bytes in the string, including NUL-termination. 
 	int length = strlen (text) + 1;
 	write (socket_fd, &length, sizeof (length));
 	// Write the string.
 	write (socket_fd, text, length);
-	std::cout<<"Write verj\n";
 }
 int server (int client_socket)
 {
         int length;
         if (!(read (client_socket, &length, sizeof (length)) == 0)) {
-	std::cout<<"While\n";
                 char* text;
-                /* First, read the length of the text message from the socket. If
-                   read returns zero, the client closed the connection. */
-               /* if (read (client_socket, &length, sizeof (length)) == 0)
-                {
-                        return 0;
-                }*/
-                /* Allocate a buffer to hold the text.*/
                 text = (char*) malloc (length);
-                // Read the text itself, and print it.
 
                 read (client_socket, text, length);
-                //if(!text=="")
                 printf ("%s\n", text);
-                /* Free the buffer. */
 
-                /* If the client sent the message "quit," we're all done.*/
                 if (!strcmp(text, "quit"))
                 {
                         free (text);
@@ -60,19 +46,14 @@ int main (/*int argc, char* const argv[]*/)
 		const char* message = m.c_str();
 		int socket_fd;
 		struct sockaddr_un name;
-		// Create the socket. 
 		socket_fd = socket (PF_LOCAL, SOCK_STREAM, 0);
-		// Store the server's name in the socket address.
 		name.sun_family = AF_LOCAL;
 		strcpy (name.sun_path, socket_name);
-		// Connect the socket.
 		connect (socket_fd, (struct sockaddr*)&name, SUN_LEN (&name));
-		// Write the text on the command line to the socket.
 		write_text (socket_fd, message);
 		server(socket_fd);
 		close (socket_fd);
 	}
-	//remove("/home/narek/Documents/Tnayin/instant_messenger/socket");
 	return 0;
 
 }
--- server.cpp
+++ server.cpp
@@ -11,20 +11,17 @@
    message, zero otherwise. */
 void write_text (int socket_fd)
 {
-	std::cout<<"Write skizb\n";
 	const char* text = "Staca";
         // Write the number of bytes in the string, including NUL-termination.
         int length = strlen (text) + 1;
         write (socket_fd, &length, sizeof (length));
         // Write the string.
         write (socket_fd, text, length);
-	std::cout<<"Write verj\n";
 }
 
 int server (int client_socket)
 {
 	while (1) {
-	std::cout<<"WHILE skizb\n";
 		int length;
 		char* text;
 		
@@ -44,7 +41,6 @@ int server (int client_socket)
 			return 1;
 		}
 		free (text);
-	std::cout<<"WHILE verj\n";
 	}
 }
 int main ()
@@ -54,39 +50,27 @@ int main ()
 	int socket_fd;
 	struct sockaddr_un name;
 	int client_sent_quit_message;
-	// Create the socket.
 
 	socket_fd = socket (PF_LOCAL, SOCK_STREAM, 0);
-	// Indicate that this is a server.
 
 	name.sun_family = AF_LOCAL;
 	strcpy (name.sun_path, socket_name);
 	bind (socket_fd,(struct sockaddr*) &name, SUN_LEN (&name));
-	// Listen for connections.
 
 	listen (socket_fd, 5);
-	/* Repeatedly accept connections, spinning off one server() to deal
-	   with each client. Continue until a client sends a "quit"
-	   message.
-	   */
+	
 	do {
 		struct sockaddr_un client_name;
 		socklen_t client_name_len;
 		int client_socket_fd;
-		// Accept a connection.
 
 		client_socket_fd = accept (socket_fd, (struct sockaddr*) &client_name, &client_name_len);
-		// Handle the connection.
 
 		client_sent_quit_message = server (client_socket_fd);
-		std::cout<<"1\n";
-		std::cout<<"2\n";
-		// Close our end of the connection.
 
 		close (client_socket_fd);
 	}
 	while (!client_sent_quit_message);
-	// Remove the socket file.
 	close (socket_fd);
 	unlink (socket_name);
 	return 0;
