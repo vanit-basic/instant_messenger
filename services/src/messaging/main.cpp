c00ad42f6a8128d388745bd0059e8927a161a344
NarekJilavyan <narek_jilavyan@mail.ru>
2019-02-25 14:46:00 +0400

    messaging main

---
+++ services/src/messaging/main.cpp
@@ -0,0 +1,33 @@
+#include <usr_interrupt_handler.hpp>
+#include <runtime_utils.hpp>
+#include <messaging/messaging.hpp>
+using namespace web;
+using namespace cfx;
+
+int main(int argc, const char * argv[])
+{
+        InterruptHandler::hookSIGINT();
+
+        Messaging server("ConfigFile.txt");
+                if(server.checkServices())
+                {
+
+                        try {
+                                // wait for server initialization...
+                                server.accept().wait();
+                                std::cout << "Router start " << std::endl;
+
+                                InterruptHandler::waitForUserInterrupt();
+
+                                server.shutdown().wait();
+                        }
+                        catch(std::exception & e) {
+                                std::cerr << "somehitng wrong happen! :(" << '\n';
+                        }
+                        catch(...) {
+                                RuntimeUtils::printStackTrace();
+                        }
+                }
+        return 0;
+}
+
