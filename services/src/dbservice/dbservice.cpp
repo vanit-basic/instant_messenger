8bc787dd6d629180a1636348c2bfa6fde43c9f5e
Mane <maneantonyan@gmail.com>
2019-02-28 14:39:33 +0400

    sign in

--- services/src/dbservice/dbservice.cpp
+++ services/src/dbservice/dbservice.cpp
@@ -161,69 +161,95 @@ void DbService::handlePost(http_request message) {
 								result = coll2.insert_one(std::move(doc_value));*/
 							json::value response = m_db->registerUser(request);
 							message.reply(status_codes::OK, response);
-						} else if (path[1] == "userLogin") {
-                                                        std::string login = request.at("login").as_string();
-                                                        std::string password = request.at("password").as_string();
-                                                        auto response = json::value::object();
+				 } else if (path[1] == "userLogin") {
+                                        std::string login = request.at("login").as_string();
+                                        std::string password = request.at("password").as_string();
+                                        auto response = json::value::object();
 
-                                                        bsoncxx::stdx::optional<bsoncxx::document::value> result =
-                                                                coll2.find_one(document{} << "login" << login
-                                                                                << "password" << password << finalize);
-                                                        if (result) {
-                                                                bsoncxx::stdx::optional<bsoncxx::document::value> infoResult =
-                                                                        coll2.find_one(document{} << "login" << request.at("login").as_string()
-                                                                                        bsoncxx::document::view doc_view{result.view()};
-                                                                                        auto id = doc_view["id"];
-                                                                                        coll1.find_one(document{} <<  "id" << id << finalize);
+                                        bsoncxx::stdx::optional<bsoncxx::document::value> result =
+                                                coll2.find_one(document{} << "login" << login
+                                                                << "password" << password << finalize);
 
-                                                                                        bsoncxx::document::view infoView{infoResult.view()};
-                                                                                        auto response = json::value::object();
+                                
+                                        if (result) {   
+                                                /*bsoncxx::stdx::optional<bsoncxx::document::value> infoResult =
+                                                        coll2.find_one(document{} << "login" << request.at("login").as_string()
+                                                                bsoncxx::document::view doc_view{result.view()};
+                                                        auto id = doc_view["id"];*/
+                                                         //auto cursor = db["restaurants"].find({});
+                                                auto infoResult = coll1.find(document{} << "login" << login << finalize);
+                                                auto builder = bsoncxx::builder::stream::document{};
+                                                auto doc = document{};
+                                                //bsoncxx::document::value doc_value;
 
-                                                                                        response["id"] = json::value::string(id);
-                                                                                        response["firstname"] = json::value::string(infoView["firstname"].as_string());
-                                                                                        response["lastname"] = json::value::string(infoView["lastname"].as_string());
-                                                                                        response["birthdate"] = json::value::string(infoView["birthdate"].as_string());
-                                                                                        response["gender"] = json::value::string(infoView["gender"].as_string());
-                                                                                        response["email"] = json::value::string(infoView["email"].as_string());
-                                                                                        response["login"] = json::value::string(infoView["login"].as_string());
-                                                                                        response["joinDate"] = json::value::string(infoView["joinDate"].as_string());
+                                                for (auto&& doc1 : infoResult) {
+                                                doc = builder << bsoncxx::to_json(doc1);
+                                                        //<< bsoncxx::builder::stream::finalize;
+                                                        std::cout << bsoncxx::to_json(doc1) << std::endl;
+                                                }
 
-                                                                                        message.reply(status_codes::OK, response);
-                                                                                        } else {
-                                                                                        bsoncxx::stdx::optional<bsoncxx::document::value> loginPassResult =
-                                                                                        coll1.find_one(document{} << "login" << request.at("login").as_string() << finalize);
-                                                                                        if (loginResult) {
-                                                                                                response["passResult"] = json::value::string("wrongPass");
-                                                                                                std::string loginDate = date();
+                                        /*      bsoncxx::document::element elementID = doc["id"];
+                                                std::string id = elementID.get_utf8().value.to_string();
 
-                                                                                                coll1.find_one(document{} << "login" << request.at("login").as_string()
-                                                                                                                bsoncxx::document::view doc_view{logiPassResult.view()};
-                                                                                                                auto id = doc_view["id"];
+                                                bsoncxx::document::element elementfn = view["firstname"];
+                                                std::string firstname = elementfn.get_utf8().value.to_string();
 
-                                                                                                                collection.update_one(document{} << id << open_document
-                                                                                                                        << "login" << login << close_document << finalize,
-                                                                                                                        document{} << "$set" << open_document <<
-                                                                                                                        document{} << "$inc" << open_document <<
-                                                                                                                        "visitCount" << 1 << close_document << finalize);
+                                                bsoncxx::document::element elementln = view["lastname"];
+                                                std::string lastname = elementln.get_utf8().value.to_string();
 
-                                                                                                                collection.update_one(document{} << id << open_document
-                                                                                                                        << "login" << login << close_document << finalize,
-                                                                                                                        document{} << "$set" << open_document <<
-                                                                                                                        "visitTime" << date() << close_document << finalize);
-                                                                                                                } else {
-                                                                                                                response["loginStatus"] = json::value::string("unknownLogin");
-                                                                                                                }
+                                                bsoncxx::document::element elementbd = view["birthdate"];
+                                                std::string birthdate = elementbd.get_utf8().value.to_string();
 
-                                                                                                                }
+                                                bsoncxx::document::element elementgn = view["gender"];
+                                                std::string gender = elementgn.get_utf8().value.to_string();
+                                                
+                                                bsoncxx::document::element elementml = view["email"];
+                                                std::string mail = elementml.get_utf8().value.to_string();
 
-                                                                                                                }
+                                                bsoncxx::document::element elementlg = view["login"];
+                                                std::string login = elementlg.get_utf8().value.to_string();
+                                                
+
+                                                response["id"] = json::value::string(id);
+                                                response["firstname"] = json::value::string(firstname);
+                                                response["lastname"] = json::value::string(lastname);
+                                                response["birthdate"] = json::value::string(birthdate);
+                                                response["gender"] = json::value::string(gender);
+                                                response["email"] = json::value::string(mail);
+                                                response["login"] = json::value::string(login);
+                                        */
+
+                                                response["id"] = infoResult["id"];
+
+                                                message.reply(status_codes::OK, response);
+
+                                        } else {
+                                                bsoncxx::stdx::optional<bsoncxx::document::value> loginPassResult =
+                                                coll2.find_one(document{} << "login" << request.at("login").as_string() << finalize);
+                                                if (loginResult) {
+                                                        response["passResult"] = json::value::string("wrongPass");
+                                                        std::string loginDate = date();
+
+                                                bsoncxx::stdx::optional<bsoncxx::document::value> info =
+                                                        coll1.find_one(document{} << "login" << login
+                                                                bsoncxx::document::view doc_view{info.view()};
+                                                coll1.update_one(document{} << "login" << login << finalize,
+                                                                document{} << "$set" << open_document <<
+                                                                document{} << "$inc" << open_document <<
+                                                                "visitCount" << 1 << close_document << finalize);
+
+                                                coll1.update_one(document{} << "login" << login
+                                                        << close_document << finalize,
+                                                        document{} << "$set" << open_document <<
+                                                        "visitTime" << date() << close_document << finalize);
+                                                } else {
+                                                        response["loginStatus"] = json::value::string("unknownLogin");
+                                                }
+
                                         }
-                                        //      else{}
+
                                 }
-                                }
-                                else
-                                {
-                                        message.reply(status_codes::NotImplemented, responseNotImpl(methods::POST));
+                        }
 
 		});
 	//	});
