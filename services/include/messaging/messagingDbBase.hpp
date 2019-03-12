e45ddb3021ad8f56d08ba648edca0211de85b576
NarekJilavyan <narek_jilavyan@mail.ru>
2019-03-12 21:54:10 +0400

    functions

--- services/include/messaging/messagingDbBase.hpp
+++ services/include/messaging/messagingDbBase.hpp
@@ -15,19 +15,20 @@ using namespace web::http::client;
 using namespace concurrency::streams;
 
 class messagingDbBase {
-	
+
 	virtual json::value getUserConversations(std::string userId) = 0;
-	virtual json::value getUsersConversation(std::string userId1, std::string userId2) = 0;
-	virtual std::string addUserMessage(std::string from, std::string to, std::string message) = 0;
-	virtual bool updateUserMessage(std::string from, std::string to, std::string messageInfo) = 0;
-	virtual bool removeUserConversation(std::string userId1, std::string userId2) = 0;
-	virtual bool removeMessage(json::value messageInfo) = 0;
-	virtual bool removeGroupConversation(std::string groupId) = 0;
-	virtual bool removeMessageFromGroupConversation(std::string groupId, std::string messageId) = 0;
-	virtual std::string getGroupConversation(std::string groupID) = 0;
-	virtual std::string addGroupMessage(std::string groupId, std::string userId, json::value message) = 0;
-	virtual bool updateGroupMessage(std::string groupId, json::value message) = 0;
+        virtual json::value getUsersConversation(std::string userId1, std::string userId2) = 0;
+        virtual std::string userSendMessage(std::string from, std::string to, std::string message) = 0;
+        virtual bool userUpdateMessage(std::string from, std::string to, std::string messageInfo) = 0;
+        virtual bool userRemoveConversation(std::string userId1, std::string userId2) = 0;
+        virtual bool userRemoveMessage(json::value messageInfo) = 0;
+       
+       	virtual bool groupRemoveConversation(std::string groupId) = 0;
+        virtual bool groupRemoveMessage(std::string groupId, std::string messageId) = 0;
+        virtual std::string getGroupConversation(std::string groupID) = 0;
+        virtual std::string groupSendMessage(std::string groupId, std::string userId, json::value message) = 0;
+        virtual bool groupUpdateMessage(std::string groupId, json::value message) = 0;
+
 };
 #endif
-
 
