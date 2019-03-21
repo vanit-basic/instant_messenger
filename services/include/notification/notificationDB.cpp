201ce66a7b74c6babd6a9769375bd842712ab22a
astgh13 <astgharakelyan97@gmail.com>
2019-03-21 14:34:22 +0400

    change name of class

---
+++ services/include/notification/notificationDB.cpp
@@ -0,0 +1,34 @@
+#ifndef NOTDB_HPP
+#define NOTDB_HPP
+
+#include <base/basic_controller.hpp>
+#include <cpprest/http_client.h>
+
+#include <cpprest/filestream.h>
+#include <bsoncxx/json.hpp>
+#include <mongocxx/client.hpp>
+#include <mongocxx/stdx.hpp>
+#include <mongocxx/uri.hpp>
+#include <mongocxx/pool.hpp>
+#include <bsoncxx/builder/stream/document.hpp>
+#include <mongocxx/instance.hpp>
+#include <cpprest/http_client.h>
+#include <cpprest/filestream.h>
+#include <bsoncxx/types.hpp>
+
+class NotificationDB {
+	public:
+		NotificationDB(){}
+		~NotificationDB(){}
+	public:
+		virtual json::value userJoinGroup(json::value) = 0;
+		virtual json::value userAcceptInvitation(json::value) = 0;
+		virtual json::value groupInviteUser(json::value) = 0;
+		virtual json::value groupAccepyUser(json::value) = 0;
+		virtual json::value newMessage(json::value) = 0;
+		virtual json::value messageFromService(json::value) = 0;
+		virtual json::value groupRemoveUser(json::value) = 0;
+		virtual json::value userLeaveGroup(json::value) = 0;
+};
+
+#endif
